022cfaa9cb78190a85fc42bce26803d9bca705be
zablotski2020 <72109229+zablotski2020@users.noreply.github.com>
2020-10-18 23:08:17 +0300

    New file

--- index.hpp
+++ index.hpp
@@ -1,13 +1,10 @@
-//
-//  index.hpp
-//  
-//
-//  Created by Ilya on 10/18/20.
-//
-
 #ifndef index_hpp
 #define index_hpp
 
 #include <stdio.h>
+
+int class{
+    cout << "Meo" << endl;
+}
 
 #endif /* index_hpp */
